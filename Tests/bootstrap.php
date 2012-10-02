fff3598843a2e1bc67d2102dd8443e46545ad9bb
Dominic Scheirlinck <dominic@varspool.com>
2012-10-02 11:33:42 +0000

    Relative autoloader for tests

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -7,5 +7,23 @@
 // Use SplClassLoader
 require_once(__DIR__ . '/SplClassLoader.php');
 
-$classLoader = new \SplClassLoader('Varspool\\PygmentsBundle', __DIR__ . '/../../..');
-$classLoader->register();
+spl_autoload_register(function ($class) {
+    $separator = '\\';
+    $extension = '.php';
+    $namespace = 'Varspool\PygmentsBundle';
+    $path = __DIR__ . '/..';
+
+    if ($namespace . $separator === substr($class, 0, strlen($namespace . $separator))) {
+        // Relative to this directory
+        $class = substr($class, strlen($namespace . $separator));
+
+        if (false !== ($lastNsPos = strripos($class, $separator))) {
+            $namespace = substr($class, 0, $lastNsPos);
+            $class = substr($class, $lastNsPos + 1);
+            $file = str_replace($separator, DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+
+        $file .= $class . $extension;
+        require $path . DIRECTORY_SEPARATOR . $file;
+    }
+});
